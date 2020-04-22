028215cf7fc4bd349a0b3f1a89c7c4d6221789f7
vinay ponnuri <vinay.ponnuri@7-11.com>
2020-04-21 23:05:47 -0500

    Initial Commit

---
+++ GitHubUsersSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubUsersSearch
+//
+//  Created by Vinay Ponnuri on 4/21/20.
+//  Copyright © 2020 Vinay Ponnuri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
